3a42e63b7dedec431aa31205d65163e3df160cae
Akinwalee <obatulafuad@gmail.com>
2024-10-22 20:49:17 -0700

    Update

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -15,8 +15,10 @@ def top_student(mongo_collection):
                     "$project": {
                         "_id": 1,
                         "$name": 1,
-                        "averageScore": {"$avg": {
-                            "$avg": "$topics.score"}
+                        "averageScore": {
+                            "$avg": {
+                            "$avg": "$topics.score"
+                                }
                             },
                         "topics": 1
                         }
