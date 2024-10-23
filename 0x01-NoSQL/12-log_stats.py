3252c39732a01424598ae2d20a6096f495f47a3d
Akinwalee <obatulafuad@gmail.com>
2024-10-22 20:00:49 -0700

    Update

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,16 +7,25 @@ Calculate the stats of the logs in Mongodb
 from pymongo import MongoClient
 
 
-with MongoClient() as client:
-    db = client.logs
+def get_stats():
+    """
+    Get the stats of logs
+    """
 
-    x = db.nginx.count_documents({})
-    print("{} logs".format(x))
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("Methods:")
-    for method in methods:
-        n = db.nginx.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, n))
+    with MongoClient() as client:
+        db = client.logs
 
-    stat = db.nginx.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(stat))
+        x = db.nginx.count_documents({})
+        print("{} logs".format(x))
+        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        print("Methods:")
+        for method in methods:
+            n = db.nginx.count_documents({"method": method})
+            print("\tmethod {}: {}".format(method, n))
+
+        stat = db.nginx.count_documents({"method": "GET", "path": "/status"})
+        print("{} status check".format(stat))
+
+
+if __name__ == "__main__":
+    get_stats()
